533091156987beeff015b130ffb2cf7b915ec578
Alvin Ng <alvinhewittalvin@gmail.com>
2019-06-24 15:20:42 -0700

    completed work

---
+++ db/seeds.sql
@@ -0,0 +1,12 @@
+-- Insert rows into table 'TableName'
+INSERT INTO burgers
+( -- columns to insert data into
+ burger_name, devoured
+)
+VALUES
+(
+ "A Whole New World Burger", TRUE
+),
+(
+ "Today Burger", FALSE
+)
