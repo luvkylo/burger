66b23e1db9643ad56479665a5f69ae4ea0fc662f
Alvin Ng <alvinhewittalvin@gmail.com>
2019-06-25 10:55:15 -0700

    updated SQL schema

--- db/schema.sql
+++ db/schema.sql
@@ -5,5 +5,6 @@ USE burgers_db;
 CREATE TABLE burgers (
     id INT AUTO_INCREMENT PRIMARY KEY NOT NULL,
     burger_name VARCHAR(100) NOT NULL,
-    devoured BOOLEAN NOT NULL
+    devoured BOOLEAN NOT NULL,
+    createdAt TIMESTAMP NOT NULL
 );
